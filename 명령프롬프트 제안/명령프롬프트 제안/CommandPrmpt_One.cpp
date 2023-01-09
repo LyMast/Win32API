53a6ac83ba5dc8f77c150069bbe5512be8f3fe49
안영훈 <lymast3578@naver.com>
2023-01-09 12:30:34 +0900

    add project

---
+++ 명령프롬프트 제안/명령프롬프트 제안/CommandPrmpt_One.cpp
@@ -0,0 +1,29 @@
+
+/*
+	CommandPrmpt_one.cpp
+	프로그램 설명: 명령 프롬프트의 골격.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>
+#include <locale.h>
+#include <windows.h>
+
+#define STR_LEN			256
+#define CMD_TOKEN_NUM	10
+
+TCHAR ERROR_CMD[]
+	= _T("'%s'은(는) 실행할 수 있는 프로그램이 아닙니다. \n");
+
+
+int tmain(int argc, TCHAR* argv[])
+{
+
+
+
+
+}
+
+                                                                                                                                    
